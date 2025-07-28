1c682b36e295b46e590fd2213feba7ee8518236e
Mujtaba <ghulammujtabapreedy@gmail.com>
2025-07-28 19:18:02 +0500

    Aboutuschanges

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-        <h1>About</h1>
+      <h1>About US</h1>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
